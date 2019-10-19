23c84757f9a5182cb72ae8115f50cbca5f173d64
Pary Ly <paleepary@aol.com>
2019-10-19 13:49:45 -0700

    added some js code

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,16 @@ function buildQueryURL() {
     console.log(queryURL)
 }
 buildQueryURL()
+
+function articlesearch() {
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=9Wd4T3TPAP92lIhIMSV6mVjMrAAelkFt";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      $("#search-term").text(JSON.stringify(response));
+    });
+    console.log(queryURL);
+};
+articlesearch ();
