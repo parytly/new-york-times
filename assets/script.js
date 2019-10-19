5c7d0d11f3147d33944024c378b0964a13c888f1
Caitlin Phillips <caitip28@gmail.com>
2019-10-19 13:44:26 -0700

    initial js

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+function buildQueryURL() {
+
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=9Wd4T3TPAP92lIhIMSV6mVjMrAAelkFt";
+    console.log(queryURL)
+}
+buildQueryURL()
